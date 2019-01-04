88db799dd780dbadec80780644f7c8c338db1e44
落日羽音 <xingxuanma@gmail.com>
2019-01-04 23:43:47 +0900

    developing UDP

--- client_udp.py
+++ client_udp.py
@@ -2,54 +2,33 @@
 UDP客户端，尝试连接服务端。
 当建立连接后发送指定数据。
 '''
-import os, json, asyncio, hashlib
+
+import os, time, json, asyncio, hashlib
 
 file = os.listdir('send')
-part = 0
-# 网速M/s
-net = 4
 ip = '192.168.1.9'
 
 # 声明一个管理类管理所传送文件的信息
-class Status:
-    def __init__(self, part, name):
-        self.part = part
-        # 传入文件绝对路径
+class FilePart:
+    def __init__(self, name, size, part, all, data):
+        # 传入文件信息
         self.name = name
-        self._size = os.path.getsize(name)
-        # 相邻两块组之间间隔时间，10块为一组
-        self._sleep_time = 0
-        # 当part设置为0时采用自动确定块数策略
-        if self.part == 0:
-            # 单块不超过每秒网速
-            self.part = round(self._size / 1048576 / net)
-            # 大于10块时间隔时间就是块数
-            if self.part > 10:
-                self._sleep_time = 5
-        else:
-            # 手动设置块数策略后期增加
-            pass
-    def get_part(self):
-        return self.part
-    def get_sleep_time(self):
-        return self._sleep_time
+        self.size = size
+        self.part = part
+        self.all = all
+        self.data = data
+        # 每块文件md5信息
+        _md5 = hashlib.md5()
+        _md5.update(self.data)
+        self.md5 = _md5.hexdigest()
 
-# 生成数据头和各数据分组
-def file_spliter(file_name, p):
-    file_size = os.path.getsize(file_name)
-    # 5M以下文件不分块
-    if file_size <= 5242880:
-        p = 1
-    md5 = hashlib.md5()
-    with open(file_name, 'rb') as f:
-        md5.update(f.read())
-        file_md5 = md5.hexdigest()
-        f.seek(0)
-        data = [f.read(file_size // p + 1) for i in range(p)]
-    # 为每个part加上头信息
-    for i in range(len(data)):
-        part_info = {'name': file_name, 'size': file_size, 'md5': file_md5, 'part': i, 'all': p}
-        data[i] = json.dumps(part_info).encode() + b'---+++header+++---' + data[i]
+# 各数据块管理类实例
+def file_spliter(name):
+    size = os.path.getsize(name)
+    # 块总数(单块限定64K)
+    all = size // 65400 + 1
+    with open(name, 'rb') as f:
+        data = [FilePart(name, size, i, all, f.read(65400)) for i in range(all)]
     return data
 
 class EchoClientProtocol:
@@ -61,13 +40,36 @@ class EchoClientProtocol:
         self.on_con_lost = loop.create_future()
     def connection_made(self, transport):
         self.transport = transport
-        # 建立连接后发送数据
-        info = json.loads(self.data.split(b'---+++header+++---')[0])
-        print('Sending file:{0} (Part {1}/{2})...'.format(info['name'], info['part'], info['all']), end='')
-        self.transport.sendto(self.data)
+        # 只有第一块时才发送建立连接消息
+        if self.data.part == 0:
+            self.transport.sendto(json.dumps({'type':'message','data':'established'}).encode())
     def datagram_received(self, message, addr):
-        # 回显接收的数据
-        print(message.decode())
+        # 等待返回是否收到，情况一说明单块收到，情况二说明MD5情况
+        # 若情况种类繁多可以考虑独立为函数，解析头部获取数据报头部与数据
+        message = json.loads(message)
+        print(message)
+        if message['type'] == 'message':
+            print({'type':'data','name':self.data.name,'size':self.data.size,'part':self.data.part,'all':self.data.all,'md5':self.data.md5})
+            # 建立连接就发送数据
+            if message['data'] == 'established':
+                fdata = json.dumps({'type':'data','name':self.data.name,'size':self.data.size,'part':self.data.part,'all':self.data.all,'md5':self.data.md5}).encode() + b'---+++data+++---' + self.data.data
+                print('Sending file:{0} (Part {1}/{2})...'.format(self.data.name, self.data.part + 1, self.data.all), end='')
+                self.transport.sendto(fdata)
+                print('Done.')
+                '''
+                # 进行到最后一块时等待MD5消息(需要新开一个数据报接收才行)
+                if self.data.part + 1 == self.data.all:
+                    while True:
+                        if message['data'] == 'MD5_passed':
+                            print('Transmission complete.')
+                            break
+                        elif message['data'] == 'MD5_failed':
+                            print('Transmission failed, MD5 checking failed.')
+                            break
+                        else:
+                            print(message['data'], 'Waiting for response...', end='\r')
+                            time.sleep(0.1)
+                '''
         self.transport.close()
     def error_received(self, exc):
         # 异常处理函数，先忽略
@@ -77,14 +79,11 @@ class EchoClientProtocol:
 
 async def main():
     loop = asyncio.get_running_loop()
-    # 欲发送的内容，应提前准备好
     for f in file:
-        status = Status(part, f)
-        data = file_spliter(f, status.get_part())
-        for d in data:
+        for part in file_spliter(f):
             transport, protocol = await loop.create_datagram_endpoint(
-                lambda: EchoClientProtocol(d, loop),
-                remote_addr=('127.0.0.1', 12345))
+                lambda: EchoClientProtocol(part, loop),
+                remote_addr=(ip, 12345))
     try:
         await protocol.on_con_lost
     finally:
