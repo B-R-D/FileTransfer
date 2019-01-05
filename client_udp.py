7c75f7b57778d9c19781d0be8907f663ac4d764d
落日羽音 <xingxuanma@gmail.com>
2019-01-05 22:44:46 +0900

    继续完善UDP服务器/客户端
    1. 服务器解决生成文件时的卡顿；
    2. 客户端解决文件发送时只能发首块的问题。
    3. 现阶段单文件可以正常发送，多文件发送仍待改进。

--- client_udp.py
+++ client_udp.py
@@ -1,6 +1,8 @@
 '''
 UDP客户端，尝试连接服务端。
 当建立连接后发送指定数据。
+问题1：多文件发送；
+问题2：优化代码
 '''
 
 import os, time, json, asyncio, hashlib
@@ -26,51 +28,45 @@ class FilePart:
 def file_spliter(name):
     size = os.path.getsize(name)
     # 块总数(单块限定64K)
-    all = size // 65400 + 1
+    all = size // 65000 + 1
     with open(name, 'rb') as f:
-        data = [FilePart(name, size, i, all, f.read(65400)) for i in range(all)]
+        data = [FilePart(name, size, i, all, f.read(65000)) for i in range(all)]
     return data
 
 class EchoClientProtocol:
     # 传入构造函数:发送的内容及asyncio循环实例
     def __init__(self, data, loop):
         self.data = data
+        self.count = 0
+        self.all = data[0].all
         self.loop = loop
         self.transport = None
         self.on_con_lost = loop.create_future()
+
     def connection_made(self, transport):
         self.transport = transport
         # 只有第一块时才发送建立连接消息
-        if self.data.part == 0:
+        if self.data[self.count].part == 0:
             self.transport.sendto(json.dumps({'type':'message','data':'established'}).encode())
+
     def datagram_received(self, message, addr):
-        # 等待返回是否收到，情况一说明单块收到，情况二说明MD5情况
-        # 若情况种类繁多可以考虑独立为函数，解析头部获取数据报头部与数据
         message = json.loads(message)
-        print(message)
         if message['type'] == 'message':
-            print({'type':'data','name':self.data.name,'size':self.data.size,'part':self.data.part,'all':self.data.all,'md5':self.data.md5})
-            # 建立连接就发送数据
-            if message['data'] == 'established':
-                fdata = json.dumps({'type':'data','name':self.data.name,'size':self.data.size,'part':self.data.part,'all':self.data.all,'md5':self.data.md5}).encode() + b'---+++data+++---' + self.data.data
-                print('Sending file:{0} (Part {1}/{2})...'.format(self.data.name, self.data.part + 1, self.data.all), end='')
+            # 最后一块发送完后等待MD5
+            if message['data'] == 'MD5_passed':
+                print('\nTransmission complete.')
+                self.transport.close()
+            elif message['data'] == 'MD5_failed':
+                print('\nTransmission failed, MD5 checking failed.')
+                self.transport.close()
+            # 非最后一块就发送数据
+            elif message['data'] == 'get':
+                fdata = json.dumps({'type':'data','name':self.data[self.count].name,'size':self.data[self.count].size,'part':self.data[self.count].part,'all':self.data[self.count].all,'md5':self.data[self.count].md5}).encode() + b'---+++data+++---' + self.data[self.count].data
+                print('Sending file:{0} (Part {1}/{2})...'.format(self.data[self.count].name, self.data[self.count].part + 1, self.data[self.count].all), end='')
                 self.transport.sendto(fdata)
-                print('Done.')
-                '''
-                # 进行到最后一块时等待MD5消息(需要新开一个数据报接收才行)
-                if self.data.part + 1 == self.data.all:
-                    while True:
-                        if message['data'] == 'MD5_passed':
-                            print('Transmission complete.')
-                            break
-                        elif message['data'] == 'MD5_failed':
-                            print('Transmission failed, MD5 checking failed.')
-                            break
-                        else:
-                            print(message['data'], 'Waiting for response...', end='\r')
-                            time.sleep(0.1)
-                '''
-        self.transport.close()
+                print('Done.', end='\r')
+                self.count += 1
+                
     def error_received(self, exc):
         # 异常处理函数，先忽略
         pass
@@ -80,10 +76,9 @@ class EchoClientProtocol:
 async def main():
     loop = asyncio.get_running_loop()
     for f in file:
-        for part in file_spliter(f):
-            transport, protocol = await loop.create_datagram_endpoint(
-                lambda: EchoClientProtocol(part, loop),
-                remote_addr=(ip, 12345))
+        transport, protocol = await loop.create_datagram_endpoint(
+            lambda: EchoClientProtocol(file_spliter(f), loop),
+          remote_addr=(ip, 12345))
     try:
         await protocol.on_con_lost
     finally:
--- server_udp.py
+++ server_udp.py
@@ -1,17 +1,73 @@
 '''
 UDP服务端，等待客户端的连接。
 有连接呼入时接收文件并保存。
+解决1：块数增多(超千)时组装文件时间过长；
+问题2：检查MD5时的回显混乱；
 '''
 import os, time, json, asyncio, hashlib
+# 全局状态
+status = {'__error':[]}
+
+def display_file_length(file_size):
+    if file_size < 1024:
+        return '{0:.1f}B'.format(file_size)
+    elif 1024 <= file_size < 1048576:
+        return '{0:.1f}kB'.format(file_size/1024)
+    elif 1048576 <= file_size < 1073741824:
+        return '{0:.1f}MB'.format(file_size/1048576)
+    else:
+        return '{0:.1f}GB'.format(file_size/1073741824)
+
+def write_data(info, data):
+    # 检查块的MD5后写入文件，添加全局块信息
+    print('Checking MD5...', end='')
+    md5 = hashlib.md5()
+    md5.update(data)
+    if md5.hexdigest() == info['md5']:
+        print('Passed.')
+        # 打开文件对象并附加数据
+        with open(info['name'], 'ab') as filedata:
+            filedata.write(data)
+        if info['name'] not in status:
+            status[info['name']] = []
+        status[info['name']].append(info['part'])
+        print('{0}(part {1}/{2}) complete.'.format(info['name'], len(status[info['name']]), info['all']), end='\r')
+        return True
+    else:
+        print('Failed.')
+        status['__error'].append(info['part'])
+        # 以后改为抛出异常
+        return False
 
 class ServerProtocol:
     def connection_made(self, transport):
         self.transport = transport
     def datagram_received(self, data, addr):
-        message = data.decode()
-        print(message)
-        # 发送数据
-        self.transport.sendto(b'Done', addr)
+        info = json.loads(data.split(b'---+++data+++---')[0])
+        if info['type'] == 'message':
+            # 回应客户端的呼入
+            if info['data'] == 'established':
+                self.transport.sendto(json.dumps({'type':'message','data':'get'}).encode(), addr)
+        elif info['type'] == 'data':
+            data = data.split(b'---+++data+++---')[1]
+            # 写入文件
+            write_data(info, data)
+            # 未全收完才回送收到消息
+            if info['all'] != len(status[info['name']]):
+                self.transport.sendto(json.dumps({'type':'message','data':'get'}).encode(), addr)
+            else:
+                if len(status['__error']) == 0:
+                    self.transport.sendto(json.dumps({'type':'message','data':'MD5_passed'}).encode(), addr)
+                else:
+                    # 以后发送的内容中附上MD5出错的块列表error_part
+                    error_part = status.pop('__error')
+                    self.transport.sendto(json.dumps({'type':'message','data':'MD5_failed'}).encode(), addr)
+                    status['__error'] = []
+                # 从状态字典中删除该key，以便传送同名文件
+                status.pop(info['name'])
+                print('\nFile: {0}({1}) transmission complete.\n'.format(info['name'], display_file_length(info['size'])))
+    def connection_lost(self, exc):
+        print('Server terminated.')
 
 async def main():
     loop = asyncio.get_running_loop()
@@ -20,7 +76,7 @@ async def main():
         local_addr=('0.0.0.0', 12345))
     print('Waiting for incoming transmission...')
     try:
-        await asyncio.sleep(3600)
+        await asyncio.sleep(300)
     finally:
         transport.close()
 
