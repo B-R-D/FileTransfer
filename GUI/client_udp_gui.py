68d607cc2fe90c60ae16e01fca81048314c68137
落日羽音 <xingxuanma@gmail.com>
2019-01-20 23:54:16 +0900

    Update client_udp_gui.py

--- GUI/client_udp_gui.py
+++ GUI/client_udp_gui.py
@@ -6,13 +6,15 @@ UDP客户端GUI版
 改进3：添加时不清除已添加，有按钮可清除；
 改进4：MD5检查错误的文件提示；
 改进5：选择文件时记住上次位置；
+解决6：按照屏幕分辨率动态设置窗口尺寸；
 '''
 import os, sys
 import clientudp
 from multiprocessing import Process
-from PyQt5.QtCore import Qt, QCoreApplication
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QDesktopWidget, QLabel, QGridLayout, QFileDialog, QFrame, QMainWindow, QAction, QMessageBox
+from PyQt5.QtCore import Qt, QCoreApplication, QSettings
 from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QGridLayout, QFrame, QMainWindow, QApplication
+from PyQt5.QtWidgets import QPushButton, QLabel, QDialog, QFileDialog, QInputDialog, QLineEdit, QAction, QMessageBox
 
 class FilePart:
     def __init__(self, name, size, part, all, data):
@@ -30,8 +32,11 @@ class ClientWindow(QMainWindow):
         
     def initUI(self):
         self.setFont(QFont('Arial', 10))
-        # 是否可以按照屏幕分辨率动态设置窗口尺寸？
-        self.resize(600, 800)
+        # 按屏幕分辨率调整窗口大小
+        self.resolution = QDesktopWidget().availableGeometry()
+        self.height = self.resolution.height()
+        self.width = self.resolution.width()
+        self.resize(self.width/6.4, self.height/2.7)
         self.center()
         
         # 添加菜单栏
@@ -40,6 +45,7 @@ class ClientWindow(QMainWindow):
         chooseAct = QAction('选择(&C)', self)
         sendAct = QAction('发送(&S)', self)
         exitAct = QAction('退出(&Q)', self)
+        
         fileMenu.addAction(chooseAct)
         fileMenu.addAction(sendAct)
         fileMenu.addSeparator()
@@ -49,26 +55,31 @@ class ClientWindow(QMainWindow):
         exitAct.triggered.connect(QCoreApplication.instance().quit)
         
         settingMenu = menubar.addMenu('设置(&S)')
-        partAct = QAction('分块(&P)', self)
-        settingMenu.addAction(partAct)
+        netAct = QAction('网络设置(&N)', self)
+        fileAct = QAction('文件设置(&F)', self)
+        
+        settingMenu.addAction(netAct)
+        settingMenu.addAction(fileAct)
+        netAct.triggered.connect(self.netSettingDialog)
+        fileAct.triggered.connect(self.fileSettingDialog)
 
-        Bselector = QPushButton('选择文件', self)    
-        flist = Bselector.clicked.connect(self.fileDialog)
+        self.Bselector = QPushButton('选择文件', self)    
+        flist = self.Bselector.clicked.connect(self.fileDialog)
         self.Lfile = QLabel('未选中文件')
         self.Lfile.setFrameStyle(QFrame.Box)
         self.Lfile.setAlignment(Qt.AlignTop)
-        Bsend = QPushButton('发送', self)
+        self.Bsend = QPushButton('发送', self)
 
-        Bsend.clicked.connect(self.fileChecker)
+        self.Bsend.clicked.connect(self.fileChecker)
         
         # 设置布局
         widget = QWidget()
         self.setCentralWidget(widget)
         grid = QGridLayout()
         grid.setSpacing(30)
-        grid.addWidget(Bselector, 1, 1, 1, 1)
+        grid.addWidget(self.Bselector, 1, 1, 1, 1)
         grid.addWidget(self.Lfile, 2, 1)
-        grid.addWidget(Bsend, 3, 1, 1, 1)
+        grid.addWidget(self.Bsend, 3, 1, 1, 1)
         widget.setLayout(grid)
         
         self.setWindowTitle('FileTransfer')       
@@ -76,7 +87,7 @@ class ClientWindow(QMainWindow):
         
     # 选择要发送的文件且显示文件名列表
     def fileDialog(self):
-        fname = QFileDialog.getOpenFileNames(self, '请选择要发送的文件', os.path.abspath('.'))
+        fname = QFileDialog.getOpenFileNames(self, '请选择文件')
         flist = ''
         if fname[0]:
             for f in fname[0]:
@@ -94,10 +105,17 @@ class ClientWindow(QMainWindow):
             sender.start()
             #sender.join()
     
+    def netSettingDialog(self):
+        self.netsetting = NetDialog()
+        self.netsetting.show()
+    
+    def fileSettingDialog(self):
+        file_at_same_time, ok = QInputDialog.getInt(self, '文件设置', '同时传送的文件数(1-3)', max=3, min=1)
+    
     # 打开窗体时位于屏幕中心
     def center(self):
         qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
+        cp = self.resolution.center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
@@ -105,12 +123,47 @@ class ClientWindow(QMainWindow):
     def keyPressEvent(self, k):
         if k.key() == Qt.Key_Escape:
             self.close()
+
+# 自定义网络设置对话框
+class NetDialog(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.settings = QSettings(os.path.join(os.path.abspath('.'), 'settings.ini'), QSettings.IniFormat)
+        self.initUI()
+
+    def initUI(self):
+        self.resize(800, 600)
         
+        self.Lip = QLabel('服务器IP')
+        self.Eip = QLineEdit(self)
+        self.Lport = QLabel('端口号(1024-65535)')
+        self.Eport = QLineEdit(self)
+        
+        self.Bconfirm = QPushButton('确定', self)
+        self.Bcancel = QPushButton('取消', self)
+        
+        grid = QGridLayout()
+        grid.setSpacing(30)
+        grid.addWidget(self.Lip, 1, 1)
+        grid.addWidget(self.Eip, 1, 2)
+        grid.addWidget(self.Lport, 2, 1)
+        grid.addWidget(self.Eport, 2, 2)
+        grid.addWidget(self.Bconfirm, 4, 1)
+        grid.addWidget(self.Bcancel, 4, 2)
+        self.setLayout(grid)
+        
+        self.setWindowTitle('设置')
+    
+    def keyPressEvent(self, k):
+        if k.key() == Qt.Key_Escape:
+            self.close()
+    
 if __name__ == '__main__':
     file = []
     file_at_same_time = 2
     error = []
     host = '192.168.1.3'
+    port = 12345
     app = QApplication(sys.argv)
     window = ClientWindow()
     sys.exit(app.exec())
